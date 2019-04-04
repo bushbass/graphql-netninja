5db33c35d17bddecf67d70059cd49dfa3fbf530d
Alex Nielsen <alex.nielsen@gmail.com>
2019-04-04 03:00:44 -0400

    finished tutorial number 12

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const app  = express()
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
 
+const app = express();
 
-app.use('/graphql', graphqlHTTP({
-    
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
-app.listen(process.env.PORT || 4000, 
-    () => console.log(`listening on port ${process.env.PORT}`)
-)
-
- 
+app.listen(process.env.PORT || 4000, () =>
+  console.log(
+    `listening on port ${process.env.PORT ? process.env.PORT : "4000"}`
+  )
+);
