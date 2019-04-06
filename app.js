28d8b4f2f60559ab4c023f3741767f5169f7fa82
Alex Nielsen <alex.nielsen@gmail.com>
2019-04-06 03:04:05 -0400

    finished lesson 26

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   "mongodb://bushbass:bd3snd@ds129442.mlab.com:29442/graphql-netninja",
